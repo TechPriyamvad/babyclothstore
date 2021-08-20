7eebf87600484b92b24266785bdf8fca9bb4c533
Technical Priyam <priyamvadofficial1@gmail.com>
2021-08-21 00:42:59 +0530

    breakdown homepage component into menu and directory component,design of menu component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage.component';
 
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div className='App'>
+      <div>
         <HomePage />
       </div>
     );
