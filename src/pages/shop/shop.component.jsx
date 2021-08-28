8d43b4efb166a8610403ed1006f4db7a66b04da7
Technical Priyam <priyamvadofficial1@gmail.com>
2021-08-28 18:59:20 +0530

    added logic to add item from shop page into cart

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-import SHOP_DATA from "./shop.data";
+import SHOP_DATA from "./shop.data.js";
 
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       collections: SHOP_DATA,
     };
@@ -14,10 +15,9 @@ class ShopPage extends React.Component {
 
   render() {
     const { collections } = this.state;
-
     return (
       <div className="shop-page">
-        {collections?.map(({ id, ...otherCollectionProps }) => (
+        {collections.map(({ id, ...otherCollectionProps }) => (
           <CollectionPreview key={id} {...otherCollectionProps} />
         ))}
       </div>
