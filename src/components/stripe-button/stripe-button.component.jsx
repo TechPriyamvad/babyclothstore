85b894d1de8a4354ec94bdd8541e009184a4583d
Technical Priyam <priyamvadofficial1@gmail.com>
2021-08-28 21:44:43 +0530

    added payment processing facility with stripe

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_51JTTi2SIVdTvGFwiznQOMDAcVCrFwEINinC1fAjON0EgvXTVV9fpJjQnrI5QQjQMN7vNyRAJWKfgfJd7ZgMf2nxY00t7znwpD1";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Succesful!");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
